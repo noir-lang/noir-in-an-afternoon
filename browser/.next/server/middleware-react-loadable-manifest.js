8fc962276d99fe8f06336f3b83d3569396d2e4ca
José Pedro Sousa <github@zepedro.me>
2023-09-20 15:12:44 +0100

    adding browser exercise

--- browser/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"../utils/noir.ts -> @aztec/bb.js\":{\"id\":\"../utils/noir.ts -> @aztec/bb.js\",\"files\":[\"static/chunks/node_modules_aztec_bb_js_dest_browser_index_js.js\"]},\"../utils/noir.ts -> @noir-lang/acvm_js\":{\"id\":\"../utils/noir.ts -> @noir-lang/acvm_js\",\"files\":[]}}"
